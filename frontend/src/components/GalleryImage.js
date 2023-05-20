73a3a12952e51c99b9fce2b64fea1ab3092cdc22
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-05-19 19:43:42 -0700

    Updated component to link to details page

--- frontend/src/components/GalleryImage.js
+++ frontend/src/components/GalleryImage.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const GalleryImage = ({ painting }) => {
-    return (
-      <div>
+  return (
+    <div>
+      <Link to={`/paintings/${painting.id}`}>
         <img
           src={process.env.PUBLIC_URL + painting.images.gallery}
           alt="Gallery"
         />
         <h2>{painting.name}</h2>
         <p>{painting.artist.name}</p>
-      </div>
-    );
-  };
+      </Link>
+    </div>
+  );
+};
 
-  export default GalleryImage;
+export default GalleryImage;
