f1f2c4d0a71a9469421b0af47885a00df245cc1d
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-05-19 19:18:34 -0700

    Added image jsx with correct absolute URL with assets in public folder

--- frontend/src/components/GalleryImage.js
+++ frontend/src/components/GalleryImage.js
@@ -3,7 +3,10 @@ import React from 'react';
 const GalleryImage = ({ painting }) => {
     return (
       <div>
-        <img src={process.env.PUBLIC_URL + '/assets/starry-night/gallery.jpg'} alt="Gallery" />
+        <img
+          src={process.env.PUBLIC_URL + painting.images.gallery}
+          alt="Gallery"
+        />
         <h2>{painting.name}</h2>
         <p>{painting.artist.name}</p>
       </div>
