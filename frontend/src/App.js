52439c636856ef2ba4103fd47339232fd1eefb23
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-05-19 19:19:53 -0700

    Added Header and Gallery components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import Gallery from './components/Gallery';
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hey there</h1>
+      <Header />
+      <Gallery />
     </div>
   );
 }
