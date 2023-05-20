d9cea1ea35619151bbc217475a4215a2106c715c
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-05-19 17:15:06 -0700

    Created Pages folder

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Home.css';
+import Gallery from '../components/Gallery';
+
+const Home = () => {
+    return (
+        <>
+            <Gallery />
+        </>
+    )
+}
+
+export default Home;
