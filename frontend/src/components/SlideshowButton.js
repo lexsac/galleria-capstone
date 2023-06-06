aacf09920efc32dfc30a5908c4d80ed038faa80f
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-06-05 17:57:58 -0700

    Created SlideshowButton component

---
+++ frontend/src/components/SlideshowButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SlideshowButton = () => {
+    return ( 
+        <button className="header__button | link-1">Start Slideshow</button>
+    );
+  };
+
+export default SlideshowButton;
