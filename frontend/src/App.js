9d7f725b6d89e2daf483372eb6ba0879e6e89299
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-06-05 17:59:33 -0700

    Added Detail component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Gallery from './components/Gallery';
-import Header from './components/Header';
 import Detail from './components/Detail';
+import Header from './components/Header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
